26730718145e06728b1cc45b41b801ee305f2d22
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-27 19:55:55 +0530

    Update CastingCallCard.tsx

--- src/components/CastingCallCard.tsx
+++ src/components/CastingCallCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { MapPin, Calendar, Film, UserCheck, CheckCircle } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
